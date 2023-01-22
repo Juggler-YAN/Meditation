0880c36fc1bdcf7d6cc7c1eafe471eac1a14f8a1
Juggler <1070591379@qq.com>
2023-01-22 13:39:02 +0800

    2023.1.22

--- 1/test.c
+++ 1/test.c
@@ -1,73 +1,37 @@
 #include <stdio.h>
 
-void menu(void);
-float getNum(void);
-void choose(char, float, float);
+double power(double, int);
+double powerCore(double, int);
 
 int main(void) {
-    char ch;
-    menu();
-    scanf("%c", &ch);
-    while (ch != 'q') {
-        float num1, num2;
-        printf("Enter first number:");
-        num1 = getNum();
-        printf("Enter Second number:");
-        num2 = getNum();
-        while (ch == 'd' && num2 == 0.0) {
-            printf("Enter a number other than 0:");
-            num2 = getNum();
-        }
-        choose(ch, num1, num2);
-        menu();
-        while (getchar() != '\n') {
-            continue;
-        }
-        scanf("%c", &ch);
+    double x;
+    int n;
+    while (scanf("%lf %d", &x, &n) == 2) {
+        printf("%f\n", power(x, n));
     }
     return 0;
 }
 
-void menu(void) {
-    printf("************************************************************\n");
-    printf("Enter the operation of your choice\n");
-    printf("a) add                       b) subtract\n");
-    printf("c) multiply                  d) divide\n");
-    printf("q) quit\n");
-    printf("************************************************************\n");
-}
-
-float getNum(void) {
-    float num;
-    char error[10];
-    while (!(scanf("%f", &num))) {
-        scanf("%s", error);
-        printf("%s is not a number.\n", error);
-        printf("Please enter a number, such as 2.5, -1.78E8, or 3:");
+double powerCore(double x, int n) {
+    if (n == 1) {
+        return x;
     }
-    return num;
+    else {
+        return x * powerCore(x, n - 1);
+    }
 }
 
-void choose(char ch, float num1, float num2) {
-    switch (ch) {
-        case 'a': {
-            printf("%.2f + %.2f = %.2f\n", num1, num2, num1 + num2);
-            break;
+double power(double x, int n) {
+    if (n > 0) {
+        return powerCore(x, n);
+    }
+    else if (n < 0) {
+        if (x == 0.0) {
+            return 0.0;
         }
-        case 's': {
-            printf("%.2f - %.2f = %.2f\n", num1, num2, num1 - num2);
-            break;
-        }
-        case 'm': {
-            printf("%.2f * %.2f = %.2f\n", num1, num2, num1 * num2);
-            break;
-        }
-        case 'd': {
-            printf("%.2f / %.2f = %.2f\n", num1, num2, num1 / num2);
-            break;
-        }
-        default: {
-            printf("Input Error!\n");
-        }
+        return 1 / powerCore(x, -n);
+    }
+    else {
+        return 1.0;
     }
 }
