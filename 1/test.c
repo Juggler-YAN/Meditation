3617e695bf3c3eccda931a223013989fdd36663f
Juggler <1070591379@qq.com>
2023-01-24 01:10:31 +0800

    2023.1.24

--- 1/test.c
+++ 1/test.c
@@ -1,70 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <math.h>
 
-#define BUFSIZE 4096
+#define SIZE 3
 
-void append(FILE*, FILE*);
+void transform(double [], double [], int, double (*)(double));
+double addOne(double);
+double minusOne(double);
+void print(double [], int);
 
-int main (int argc, char * argv []) {
-    FILE *fa, *fs;
-    int files = 0;
-    int ch;
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s targetfile sourcefile[s]\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    if ((fa = fopen(argv[1], "a+")) == NULL) {
-        fprintf(stderr, "Can't open %s.", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-    if (setvbuf(fa, NULL, _IOFBF, BUFSIZE) != 0) {
-        fputs("Can't create input buffer\n", stderr);
-        exit(EXIT_FAILURE);
-    }
-    for (int i = 2; i < argc; i++){
-        if (strcmp(argv[1], argv[i]) == 0) {
-            fputs("Can't append file to itself.\n", stderr);
-            exit(EXIT_FAILURE);
-        }
-        else if ((fs = fopen(argv[i],"r")) == NULL){
-            fprintf(stderr, "Can't open %s.", argv[i]);
-            exit(EXIT_FAILURE);
-        }
-        else {
-            if (setvbuf(fs, NULL, _IOFBF, BUFSIZE) != 0) {
-                fputs("Can't create input buffer\n", stderr);
-                continue;
-            }
-            append(fs, fa);
-            if (ferror(fs) != 0) {
-                fprintf(stderr, "Error in reading file %s.\n", argv[1]);
-                exit(EXIT_FAILURE);
-            }
-            if (ferror(fa) != 0) {
-                fprintf(stderr, "Error in reading file %s.\n", argv[i]);
-                exit(EXIT_FAILURE);
-            }
-            fclose(fs);
-            files++;
-        }
-    }
-    printf("Done appending. %d files appended.\n", files);
-    rewind(fa);
-    printf("%s contents:\n", argv[1]);
-    while ((ch = getc(fa)) != EOF){
-        putchar(ch);
-    }
-    puts("\nDone displaying.");
-    fclose(fa);
-    return 0;
+int main(void) {
+    double a[SIZE] = {1, 2, 3};
+    double b[SIZE] = {0, 0, 0};
+    print(b, SIZE);
+    transform(a, b, SIZE, sin);
+    print(b, SIZE);
+    transform(a, b, SIZE, cos);
+    print(b, SIZE);
+    transform(a, b, SIZE, addOne);
+    print(b, SIZE);
+    transform(a, b, SIZE, minusOne);
+    print(b, SIZE);
+	return 0;
 }
 
-void append(FILE * source, FILE * dest) {
-    size_t bytes;
-    static char temp[BUFSIZE];
-
-    while ((bytes = fread(temp, sizeof(char), BUFSIZE, source)) > 0) {
-        fwrite(temp, sizeof(char), bytes, dest);
+void transform(double a[], double b[], int n, double (*fun)(double)) {
+    for (int i = 0; i < n; i++) {
+        b[i] = fun(a[i]);
     }
+}
+
+double addOne(double a) {
+    return a+1.0;
+}
+
+double minusOne(double a) {
+    return a-1.0;
+}
+
+void print(double a[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%.2lf ", a[i]);
+    }
+    printf("\n");
 }
