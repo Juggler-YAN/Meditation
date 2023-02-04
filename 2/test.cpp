060a488c70ef8e439ec679ab15529cd4d48a840c
Juggler <1070591379@qq.com>
2023-02-04 15:24:52 +0800

    2023.2.4

--- 2/test.cpp
+++ 2/test.cpp
@@ -1,41 +1,35 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
 #include <string>
+#include <stack>
 
 using namespace std;
 
-struct PersonInfo {
-    string name;
-    vector<string> phones;
-};
-
 int main() {
-	string line, word;
-	vector<PersonInfo> people;
-	istringstream record;
-    ifstream in("./test.txt");
-
-	while(getline(in, line)) {
-		record.str(line);
-		PersonInfo info;
-		record >> info.name;
-		while(record >> word)
-			info.phones.push_back(word);
-		record.clear();
-		people.push_back(info);
-	}
-
-	for(const auto &person : people) {
-		cout << person.name << "  ";
-		for(const auto &ph : person.phones) {
-			cout << ph << " ";
-		}
-		cout << endl;
-	}
-
-	in.close();
-
-	return 0;
+    string s{"Hello,(world)!"};
+    stack<char> stk;
+    bool flag = false;
+    for (const auto &i : s) {
+        if (i == '(') {
+            flag = true;
+            continue;
+        }
+        else if (i == ')') {
+            flag = false;
+        }
+        if (flag) stk.push(i);
+    }
+    string rep;
+    while (!stk.empty()) {
+        rep.push_back(stk.top());
+        stk.pop();
+    }
+    for (decltype(rep.size()) i = 0; i != rep.size() / 2; ++i) {
+        char temp;
+        temp = rep[i];
+        rep[i] = rep[rep.size()-i-1];
+        rep[rep.size()-i-1] = temp;
+    }
+    s.replace(s.find("("), rep.size()+2, rep);
+    cout << s << endl;
+    return 0;
 }
